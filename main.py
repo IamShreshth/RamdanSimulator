f601ecd106db7a6366a11abd3c5fe2ba3732395e
IamShreshth <shreshthdhimole@gmail.com>
2026-03-20 16:33:25 +0530

    Basic Struture fof the simulator

---
+++ main.py
@@ -0,0 +1,57 @@
+import pygame
+import random
+
+pygame.init()
+#Dimensions of the screen and player postioton and all that , Damn nigaa
+screen =pygame.display.set_mode((800,600))
+player_pos=[400,500]
+food_pos=[random.randint(0, 750), 0]
+#intial score 
+score=0
+font = pygame.font.Font(None, 36)
+#clock.tick(60) tells Pygame: "Hey, wait a second... make sure this loop only runs 60 times per second.
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    screen.fill((135, 206, 235))   #Color of the sky DAMNNNNNNN
+
+    #Event Handling
+    #1.ON/OFF Screen Close/Open Window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running=False
+
+    #controls
+    keys=pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and player_pos[0] > 0:
+        player_pos[0] -= 5
+    if keys[pygame.K_RIGHT] and player_pos[0] < 750:
+        player_pos[0] += 5
+    
+    #2.Gravity 
+    food_pos[1] += 5 # Make the food fall
+
+    # Reset food if it hits bottom or is caught
+    if food_pos[1] > 600:
+        food_pos = [random.randint(0, 750), 0]
+    
+    # Simple collision box check
+    if (player_pos[0] < food_pos[0] < player_pos[0] + 50) and \
+       (player_pos[1] < food_pos[1] < player_pos[1] + 50):
+        score += 1
+        food_pos = [random.randint(0, 750), 0]
+
+    #score board 
+    score_board = font.render(f"Iftar Items: {score}", True, (255, 255, 255))
+    screen.blit(score_board, (10, 10))
+
+    # 3. Rendering (Draw everything)
+    pygame.draw.rect(screen, (255, 0, 0), (player_pos[0], player_pos[1], 50, 50)) # Player
+    pygame.draw.circle(screen, (255, 255, 0), (food_pos[0], food_pos[1]), 15)   # Food
+    
+    pygame.display.flip()
+    clock.tick(60) # Limits to 60 FPS
+
+pygame.quit()
+
